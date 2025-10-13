093c3b62c73835aa86fc384927386ad47a70f9da
z10-arch <234732700+z10-arch@users.noreply.github.com>
2025-10-14 03:04:33 +0530

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.transparency")
+-- Enable OSC52 clipboard (works over SSH)
+vim.g.clipboard = 'osc52'
+
+-- Use system clipboard for all yank/paste operations
+vim.opt.clipboard = 'unnamedplus'
