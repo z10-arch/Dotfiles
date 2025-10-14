daf83f850e6acfde98d851b31269c52638c2bd03
z10-arch <234732700+z10-arch@users.noreply.github.com>
2025-10-14 12:57:43 +0530

    Update multicursor.lua
    Multi cursor updated No Lagging tested working.

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -1,15 +1,38 @@
+-- Multi-cursor plugin for LazyVim
+-- Save this as: nvim/lua/plugins/multicursor.lua
+
 return {
   {
-    "terryma/vim-multiple-cursors",
-    keys = {
-      { "<C-n>", mode = { "n", "v" } },
-    },
+    "mg979/vim-visual-multi",
+    branch = "master",
+    event = "VeryLazy",
     init = function()
-      vim.g.multi_cursor_use_default_mapping = 0
-      vim.g.multi_cursor_start_word_key = "<C-n>"
-      vim.g.multi_cursor_next_key = "<C-n>"
-      vim.g.multi_cursor_skip_key = "<C-x>"
-      vim.g.multi_cursor_quit_key = "<Esc>"
+      -- VSCode-like keybindings
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-d>",           -- Ctrl+D to select next word
+        ["Find Subword Under"] = "<C-d>",   -- Same for partial words
+        ["Add Cursor Down"] = "<C-Down>",   -- Ctrl+Down to add cursor below
+        ["Add Cursor Up"] = "<C-Up>",       -- Ctrl+Up to add cursor above
+        ["Select All"] = "<C-S-l>",         -- Ctrl+Shift+L to select all
+        ["Mouse Cursor"] = "<C-LeftMouse>", -- Ctrl+Click to add cursor
+        ["Skip Region"] = "q",              -- Skip current selection
+        ["Remove Region"] = "Q",            -- Remove current cursor
+        ["Undo"] = "u",
+        ["Redo"] = "<C-r>",
+      }
+
+      -- Settings
+      vim.g.VM_theme = "ocean"
+      vim.g.VM_highlight_matches = "underline"
+      vim.g.VM_silent_exit = 1
+      vim.g.VM_set_statusline = 2
+      vim.g.VM_show_warnings = 0
+
+      -- Custom highlights
+      vim.g.VM_Mono_hl = "DiffText"
+      vim.g.VM_Extend_hl = "DiffAdd"
+      vim.g.VM_Cursor_hl = "Visual"
+      vim.g.VM_Insert_hl = "DiffChange"
     end,
   },
 }
